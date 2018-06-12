380679a983bb1a8d63646cb9d22de30449073863
Atil Samancioglu <samancioglu.atil@gmail.com>
2018-06-12 21:39:53 +0300

    Initial commit

---
+++ app/src/main/java/com/atilsamancioglu/broadcastreceiverpermissionapp2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.atilsamancioglu.broadcastreceiverpermissionapp2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signal (View view) {
+
+        Intent intent = new Intent("my.signal");
+
+        sendBroadcast(intent,"my.permission");
+
+    }
+}
